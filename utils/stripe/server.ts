ca3ce1885d94f842f45c6d31e5bae4010c74624b
Heaven Ike <76069475+heavenike02@users.noreply.github.com>
2025-02-22 11:52:40 +0000

    Add stripe client

---
+++ utils/stripe/server.ts
@@ -0,0 +1,14 @@
+import Stripe from "stripe";
+
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+
+if (!stripeSecretKey) {
+  throw new Error(
+    "STRIPE_SECRET_KEY is not defined in the environment variables."
+  );
+}
+
+const stripe = new Stripe(stripeSecretKey);
+
+export { stripe };
